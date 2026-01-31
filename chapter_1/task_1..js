038b4413585b9e4d394bef8c2bc202e9b414e68f
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-01-31 15:44:23 +0900

    test select task

---
+++ chapter_1/task_1..js
@@ -0,0 +1,9 @@
+const myName = "dmeetri";
+const progLang = "TypeScript";
+const courseCreatorName = "Nikita";
+const reasonText = "хочу зарабатывать";
+const numberOfMonth = 12;
+
+function task_1() {
+  return `Привет! Меня зовут ${myName}. Я выбрал ${progLang} потому что ${reasonText}. Меня обучает ${courseCreatorName} на протяжении ${numberOfMonth}`;
+}
