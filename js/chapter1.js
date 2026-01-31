11487fc676923ba8f75b886a666c468f47a24719
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-01-31 16:15:47 +0900

    ch1 - tsk-2 tsk-3

--- js/chapter1.js
+++ js/chapter1.js
@@ -7,3 +7,17 @@ const numberOfMonth = 12;
 function ch1_task1() {
   return `Привет! Меня зовут ${myName}. Я выбрал ${progLang} потому что ${reasonText}. Меня обучает ${courseCreatorName} на протяжении ${numberOfMonth}`;
 }
+
+function ch1_task2() {
+  let myInfoText =
+    "Привет! Меня зовут dmeetri. Я выбрал TypeScript потому что хочу зарабатывать. Меня обучает Nikita на протяжении 12";
+  return myInfoText.replaceAll("TypeScript", "TYPESCRIPT");
+}
+
+function ch1_task3() {
+  let userName = prompt("Пиши сюда:").toLowerCase();
+  userName = userName.trim();
+
+  alert(userName);
+  return userName;
+}
