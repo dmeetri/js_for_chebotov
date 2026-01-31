8f917b5b7b7fc4e9fad966fa93d2d58c58d5de05
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-01-31 16:54:58 +0900

    ср1 еыл6

--- js/chapter1.js
+++ js/chapter1.js
@@ -18,12 +18,11 @@ function ch1_task3() {
   let userName = prompt("Пиши сюда:").toLowerCase();
   userName = userName.trim();
 
-  alert(userName);
   return userName;
 }
 
 function ch1_task4() {
-  let userAge = prompt("Возраст сюда йоу").trim();
+  const userAge = prompt("Возраст сюда йоу").trim();
   return parseInt(userAge);
 
   //Я знаю про унарный способ +"123" и про Number()
@@ -35,14 +34,28 @@ function ch1_task5() {
   alert("Делаем обрезку строки или, сами придумайте. ОК нажми это перетнзия");
 
   let userString = prompt("Сюда пиши текст").trim();
-  let startObrezanie = parseInt(prompt("откда начинаем резать?").trim());
-  let countObrezanie = parseInt(prompt("сколько режем?").trim());
+  const startObrezanie = parseInt(prompt("откда начинаем резать?").trim());
+  const countObrezanie = parseInt(prompt("сколько режем?").trim());
 
   return userString.slice(startObrezanie, startObrezanie + countObrezanie);
   //я крч не понял до конца суть задания, но сейчас скрипт как бы вырезает нужную область с того момента как начали и докуда закончили
   // пример - 1234567890 > с 3 по 5 > ответ > 45678
 }
 
-function ch1_task6() {}
+function ch1_task6() {
+  let userText = prompt("Сюда писать текста много").toLowerCase().split(" ");
+  const word = prompt(
+    "Вот текст написал, слово вспомни из него, которое бесит и напиши сюда. Напишите",
+  )
+    .toLowerCase()
+    .split(" ")[0]; // если что это не нейронка, у меня Pretier стоит
+
+  const index = userText.indexOf(word);
+
+  if (index === -1) return "Слово не найдено в тексте";
+
+  return userText.slice(index + 1).join(" ");
+}
 
 function ch1_task7() {}
+//крч мне было очень скучно это писать так что я развлекался как мог
