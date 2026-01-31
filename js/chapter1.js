9444bca6dc897c00003374d45f5e9617996e0439
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-01-31 15:54:21 +0900

    ch1 task1

--- js/chapter1.js
+++ js/chapter1.js
@@ -4,6 +4,6 @@ const courseCreatorName = "Nikita";
 const reasonText = "хочу зарабатывать";
 const numberOfMonth = 12;
 
-function task_1() {
+function ch1_task1() {
   return `Привет! Меня зовут ${myName}. Я выбрал ${progLang} потому что ${reasonText}. Меня обучает ${courseCreatorName} на протяжении ${numberOfMonth}`;
 }
