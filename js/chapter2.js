1a3f087c764bc2bf25cc359fe039ef993de84366
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-02-06 11:27:12 +0900

    add ch2 tsk1

--- js/chapter2.js
+++ js/chapter2.js
@@ -1,1 +1,42 @@
-function ch2_tsk1() {}
+function ch2_tsk1() {
+  const exsistLogin = "the_best_user";
+  const exsitcPassword = "12345678";
+
+  const login = prompt("Введите логин:").trim();
+  const pass = prompt("Введите пароль:").trim();
+
+  if (login === exsistLogin && pass === exsitcPassword) {
+    alert("Успешная авторизация!");
+    return "ok";
+  } else {
+    alert("Ага, обазательно");
+    return "ne ok";
+  }
+}
+
+function ch2_tsk2() {
+  const arr = {
+    "2+2=?": "4",
+    "2*2=?": "4",
+    "Было 5. 3 съели. 1 отдали. Сколько осталось?": "1",
+    "Было 10. 2 съели, 1 отдали. Дали еще 5. Сколько?": "12",
+    "2+2*2=?": "6",
+  };
+
+  let correctAnswer = 0;
+  let incorrectAnswer = 0;
+
+  for ()
+}
+
+function ch2_tsk3() {}
+
+function ch2_tsk4() {}
+
+function ch2_tsk5() {}
+
+function ch2_tsk6() {}
+
+function ch2_tsk7() {}
+
+function ch2_tsk8() {}
